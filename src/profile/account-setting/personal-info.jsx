ade6aa4a9c9c56f80d9b8d25cd998c2576a42f10
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2021-03-18 22:43:52 +0330

    added some css and buttons

--- src/profile/account-setting/personal-info.jsx
+++ src/profile/account-setting/personal-info.jsx
@@ -1,9 +1,11 @@
 import React, { Component } from 'react';
 import { BsPeopleCircle } from "react-icons/bs";
 import { IconContext } from "react-icons";
-import PhoneInput from 'react-phone-input-2'
+import PhoneInput from 'react-phone-input-2';
 import 'react-phone-input-2/lib/style.css';
 import './personal-info.css';
+import * as EmailValidator from 'email-validator';
+ 
 
 class PersonalInfo extends Component {
     constructor(props) {
@@ -15,11 +17,12 @@ class PersonalInfo extends Component {
             firstName: "",
             lastName:"",
             nationalId: "",
-            country: "",
             gender: "",
             dateOfBirth: "",
             phoneNum: "",
             email: "",
+            emailValidationMsg: "Email is not correct",
+            emailValidationError: false,
         }; 
 
     }
@@ -29,7 +32,7 @@ class PersonalInfo extends Component {
         let name = target.name;
         let value = target.value
         this.setState({
-         [name]:value
+         [name]: value
         });
         if (name === "bio"){
          let currentText = e.target.value;
@@ -38,6 +41,16 @@ class PersonalInfo extends Component {
          let charsPerPageCount = this.state.charsPerPage;
          let unitCount = Math.round(characterCount/charsPerPageCount);
          this.setState({pageCount: unitCount});
+        }
+        if(name === "email" && !EmailValidator.validate(value)){
+            this.setState({
+                emailValidationError: true,
+            });
+        }
+        if(name === "email" && EmailValidator.validate(value)){
+            this.setState({
+                emailValidationError: false,
+            });
         }
    }
 
@@ -53,91 +66,128 @@ class PersonalInfo extends Component {
                 </div>
 
                 <hr className="personalInfo-line"/>
-                
-                <div className="personalInfo-form ml-4 mt-4">
-                    <div className="firstName mb-2">
-                            <label for="firstName">First Name :</label>
-                            <input name="firstName" value={this.state.firstName} onChange={this.handleChange} id="firstName" />
-                    </div>
-
-                    <hr className="personalInfo-line"/>
-
-                    <div className="lastName mb-2">
-                            <label for="lastName">Last Name :</label>
-                            <input name="lastName" value={this.state.lastName} onChange={this.handleChange} id="lastName" />
-                    </div>
-
-                    <hr className="personalInfo-line mb-2"/>
-
-                    <div className="nationalId">
-                        <label for="nationalCode">National Code :</label>
-                        <input name="nationalId" value={this.state.nationalId} onChange={this.handleChange} id="nationalCode" />
-                    </div>
-
-                    <hr className="personalInfo-line"/>
-
-                    <div className="Country mb-2">
-                        <label for="nationality">Country :</label>
-                        <select name="country" value={this.state.country} id="nationality">
-                            <option>Iran</option>
-                            <option>Iraq</option>
-                            <option>Other</option>
-                        </select>
-                    </div>
-
-                    <hr className="personalInfo-line"/>
-
-                    <div className="Gender mb-2">
-                        <label for="Gender">Gender :</label>
-                        <select name="gender" value={this.state.gender} id="Gender">
-                            <option>Male</option>
-                            <option>Female</option>
-                            <option>Other</option>
-                        </select>
-                    </div>
-
-                    <hr className="personalInfo-line"/>
-
-                    <div className="dateOfBirth mb-2">
-                        <label for="birth">Date Of Birth :</label>
-                        <input name="dateOfBirth" value={this.state.dateOfBirth} onChange={this.handleChange} id="birth"/>
-                    </div>
-
-                    <hr className="personalInfo-line"/>
-
-                    <div className="phoneNum mb-2 row">
-                        <label for="phone" className="col-2">Phone Number :</label>
-                        <div className="col-8">
-                        <PhoneInput
-                        country={'us'}
-                        value={this.state.phoneNum}
-                        onChange={phone => this.setState({ phone })}
-                        />
+                <form class="row g-3 needs-validation" novalidate>
+                    <div className="personalInfo-form w-100 ml-4 mt-4">
+                        <div className="firstName mb-2">
+                                <label for="personalInfo-firstName">First Name :</label>
+                                <input name="firstName" value={this.state.firstName} onChange={this.handleChange} id="firstName" required/>
+                                <div class="valid-feedback">
+                                    Looks good!
+                                </div>
+                                <div class="invalid-feedback">
+                                    Please enter your firstName
+                                </div>
                         </div>
-                        {/* <input name="phoneNum" value={this.state.phoneNum} onChange={this.handleChange} id="phone" /> */}
 
-                    </div>
+                        <hr className="personalInfo-line"/>
 
-                    <hr className="personalInfo-line"/>
+                        <div className="personalInfo-lastName mb-2">
+                                <label for="lastName">Last Name :</label>
+                                <input name="lastName" value={this.state.lastName} onChange={this.handleChange} id="lastName" required/>
+                                <div class="valid-feedback">
+                                    Looks good!
+                                </div>
+                                <div class="invalid-feedback">
+                                    Please enter your lasrName
+                                </div>
+                        </div>
 
-                    <div className="emailId mb-2">
-                        <label for="email">Email :</label>
-                        <input name="email" value={this.state.email} onChange={this.handleChange} id="email" />
+                        <hr className="personalInfo-line mb-2"/>
 
-                    </div>
+                        <div className="personalInfo-nationalId">
+                            <label for="nationalCode">National Code :</label>
+                            <input name="nationalId" value={this.state.nationalId} onChange={this.handleChange} id="nationalCode" required/>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                    Please enter your National code
+                            </div>
+                        </div>
 
-                    <hr className="personalInfo-line"/>
+                        <hr className="personalInfo-line"/>
 
-                    <div className="bioField">
-                        <label for="bio">Bio :</label>
-                        <textarea id="ptextArea" name="bio" className="bio" value={this.state.bio} onChange={this.handleChange} maxlength="175" rows="4" cols="64">
-                                
-                        </textarea>
-                        <div className="textAreaCounter">
-                                {this.state.pageCount} of 175
+                        <div className="personalInfo-Gender mb-2">
+                            <label for="Gender">Gender :</label>
+                            <select name="gender" value={this.state.gender} onChange={this.handleChange} id="Gender" required>
+                                <option>Male</option>
+                                <option>Female</option>
+                                <option>Other</option>
+                            </select>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                Please select your Gender
+                            </div>
+                        </div>
+
+                        <hr className="personalInfo-line"/>
+
+                        <div className="personalInfo-dateOfBirth mb-2">
+                            <label for="birth">Date Of Birth :</label>
+                            <input name="dateOfBirth" value={this.state.dateOfBirth} onChange={this.handleChange} id="birth"/>
+                        </div>
+
+                        <hr className="personalInfo-line"/>
+
+                        <div className="personalInfo-phoneNum mb-2 row">
+                            <label for="phone" className="col-2">Phone Number :</label>
+                            <div className="col-8">
+                            <PhoneInput
+                            country={'us'}
+                            value={this.state.phoneNum}
+                            onChange={phone => this.setState({ phone })}
+                            required
+                            />
+                            </div>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                Please enter your phone number
+                            </div>
+                            {/* <input name="phoneNum" value={this.state.phoneNum} onChange={this.handleChange} id="phone" /> */}
+
+                        </div>
+
+                        <hr className="personalInfo-line"/>
+
+                        <div className="personalInfo-emailId mb-2">
+                            <label for="email">Email :</label>
+                            <input name="email" value={this.state.email} onChange={this.handleChange} id="email" required/>
+                            <div class="valid-feedback">
+                                Looks good!
+                            </div>
+                            <div class="invalid-feedback">
+                                Please enter valid email
+                            </div>
+                            {/* <div>
+                                {this.state.emailValidationError? <div>{this.state.emailValidationMsg}</div> : ''}
+                            </div> */}
+                        </div>
+
+                        <hr className="personalInfo-line"/>
+
+                        <div className="personalInfo-bioField">
+                            <label for="bio">Bio :</label>
+                            <textarea id="ptextArea" name="bio" className="bio" value={this.state.bio} onChange={this.handleChange} maxlength="175" rows="4" cols="66">
+                                    
+                            </textarea>
+                            <div className="personalInfo-textAreaCounter">
+                                    {this.state.pageCount} of 175
+                            </div>
+                        </div>
+                        <div className="personalInfo-btn mb-2 mt-2 row">
+                            <div className="submitBtn col">
+                                <button>Submit</button>
+                            </div>
+                            <div className="cancelBtn col">
+                                <button>Cancel</button>
+                            </div>
                         </div>
                     </div>
-                </div>
+                </form>
             </div>
          );
     }
