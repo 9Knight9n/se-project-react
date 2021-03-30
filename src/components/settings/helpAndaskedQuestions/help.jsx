23908b3dea6c70a7f7849116dc47c68d917b3e10
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2021-03-30 18:59:09 +0430

    found some icon and edited colors

--- src/components/settings/helpAndaskedQuestions/help.jsx
+++ src/components/settings/helpAndaskedQuestions/help.jsx
@@ -3,6 +3,9 @@ import Card from '../../cardBox';
 import travelImg from '../img/travel.png';
 import hostingImg from '../img/villa.png';
 import cookingImg from '../img/cooking.png';
+import paymentImg from '../img/debit-card.png';
+import problemImg from '../img/problem.png';
+import connectionImg from '../img/social-media.png';
 import './help.css'
 class Help extends Component {
     constructor(props) {
@@ -25,13 +28,13 @@ class Help extends Component {
                         <Card text="Learn how to host an exprience with your friends." title="Hosting experiences" img={cookingImg} />
                     </div>
                     <div className="col mt-4">
-                        <Card text="Learn how to pay to your host and what tips are best to follow." title="payemnt tips" img={cookingImg} />
+                        <Card text="Learn how to pay to your host and what tips are best to follow." title="payemnt tips" img={paymentImg} />
                     </div>
                     <div className="col mt-4">
-                        <Card text="Learn how to report your problems about your host or passenger." title="report a problem" img={cookingImg} />
+                        <Card text="Learn how to report your problems about your host or passenger." title="report a problem" img={problemImg} />
                     </div>
                     <div className="col mt-4">
-                        <Card text="Learn how to make connections and find your friends on airbnb." title="connect with people" img={cookingImg} />
+                        <Card text="Learn how to make connections and find your friends on airbnb." title="connect with people" img={connectionImg} />
                     </div>
                 </div>
                 
