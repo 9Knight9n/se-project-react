d7abf52bde91b0a7434c9f5a5ec5846b5de4ad53
abed ebadi <73102057+abed-eb@users.noreply.github.com>
2021-03-31 16:46:28 +0430

    Update term&conditions.jsx

--- src/components/settings/termsAndConditions/term&conditions.jsx
+++ src/components/settings/termsAndConditions/term&conditions.jsx
@@ -5,35 +5,36 @@ class TermsConditions extends Component {
     render() { 
         return ( 
             <div className="terms-main">
-                <h1>Generic Terms and Conditions template</h1>
+                <h1>SweetHome Terms and Conditions</h1>
                 <hr className="terms-line"/>
-                <h4 className="mt-2 mb-2">Terms and Conditions ("Terms")</h4>
+                <h4 className="mt-2 mb-2">Terms and Conditions</h4>
                 <p>
                 Last updated: (3/20/2021)
-                Please read these Terms and Conditions ("Terms", "Terms and Conditions") carefully before using the <a href="#" target="blank"> http://www.SweetHome.com </a>
-                 website and the SweetHome mobile application (the "Service") operated by SweetHome
-                  ("us", "we", or "our").
+                Please read these Terms and Conditions carefully before using the <a href="#" target="blank"> http://www.SweetHome.com </a>
+                 website and the SweetHome mobile application operated by SweetHome.
                 Your access to and use of the Service is conditioned on your acceptance of and compliance with these Terms. These Terms apply
                  to all visitors, users and others who access or use the Service.
                 </p>
-                <b>
+                <b style={{color: "red"}}>
+                    
                     By accessing or using the Service you agree to be bound by these Terms. If you disagree with any part of the terms then you may
                      not access the Service.
+                     
                 </b>
                 <hr className="terms-line"/>
                 <b>Purchases</b>
                 <p>
-                    If you wish to purchase any product or service made available through the Service ("Purchase"), you may be asked to supply certain
+                    If you wish to purchase any product or service made available through the Service , you may be asked to supply certain
                     information relevant to your Purchase including, without limitation, your …
                 </p>
                 <b>Subscriptions</b>
                 <p>
-                Some parts of the Service are billed on a subscription basis ("Subscription(s)"). You will be billed in advance on a recurring ...
+                Some parts of the Service are billed on a subscription basis. You will be billed in advance on a recurring ...
                 </p>
                 <b>Content</b>
                 <p>
                 Our Service allows you to post, link, store, share and otherwise make available certain information, text, graphics, videos, or
-                other material ("Content"). You are responsible for the …
+                other Content. You are responsible for the …
                 </p>
                 <b>Links To Other Web Sites</b>
                 <p>Our Service may contain links to third-party web sites or services that are not owned or controlled by SweetHome.
