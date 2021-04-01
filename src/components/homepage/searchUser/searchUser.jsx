e40c63789e5920e44e225699835f1994fea6e2b2
Sajad <naghizadehmdsd@gmail.com>
2021-04-01 19:49:56 +0430

    added search result items

--- src/components/homepage/searchUser/searchUser.jsx
+++ src/components/homepage/searchUser/searchUser.jsx
@@ -1,6 +1,14 @@
 import React, {Component,Fragment} from 'react';
 import './searchUser.css'
 import {Link} from "react-router-dom";
+import SearchUserResult from "./searchUserResult";
+import icon from '../../../assets/img/default-profile-picture.jpg'
+import EllipsisToolTip from "ellipsis-tooltip-react-chan";
+
+const ellipsisToolTipOptions = {
+    effect: "solid",
+    place: "top",
+}
 
 class SearchUser extends Component {
     constructor(props) {
@@ -8,13 +16,10 @@ class SearchUser extends Component {
     }
 
     state = {
-        minError:false,
         focused:false,
-        panelOpened:false,
-        searchInput:"",
-        suggestions:[],
-        informed:false,
-        activeNav:this.props.activeNav,
+        panelOpened:true,
+        searchInput:"knidwdwddwdwdwwdwwdwdwdw",
+        suggestions:[{email:"9knight9n@gmail9knight9n@gmail.com .com",profile:icon,isFriend:true},{email:"sajad@te.st",profile:icon,isFriend:false}],
     }
 
     componentDidMount(){
@@ -27,24 +32,25 @@ class SearchUser extends Component {
     handleClick=(e)=>{
         if(!document.getElementById("search-user").contains(e.target))
         {
-
             this.setState({focused:false,panelOpened:false,searchInput:""})
         }
     }
 
-    handelSearch=()=>{
+    handleSearch=()=>{
         if(!this.state.focused)
         {
             let input = document.getElementById('search-input-field');
             input.focus();
             input.select();
         }
+        else
+            this.setState({panelOpened:false,searchInput:""})
         this.setState({focused:!this.state.focused})
 
     }
 
 
-    handelInputChange=(event)=>{
+    handleInputChange=(event)=>{
         let input = event.target.value;
         this.setState({searchInput:input})
         if (input.length >2)
@@ -88,18 +94,37 @@ class SearchUser extends Component {
     render() {
         return (
             <Fragment>
-                <div id='search-user' className="d-flex flex-row">
-                    <div id='bar' className={"ml-auto d-flex flex-row-reverse".concat(this.state.focused?" active ":"")}>
-                        <input value={this.state.searchInput}
-                               onChange={this.handelInputChange} onKeyDown={this._handleKeyDown}
-                               className=" form-control shadow-none" placeholder="Search" id="search-input-field"/>
+                <div id='search-user' className="d-flex flex-row col-xl-4 col-lg-6 col-md-8 col-sm-12">
+                    <div style={{width:'inherit'}}>
+                        <div id='bar' className={"ml-auto d-flex flex-row-reverse".concat(this.state.focused?" active ":"")}>
+                            <input value={this.state.searchInput}
+                                   onChange={this.handleInputChange} onKeyDown={this._handleKeyDown}
+                                   className=" form-control shadow-none" placeholder="Search" id="search-input-field"/>
+                        </div>
+                        <div id='panel' className={"mt-2 ".concat(this.state.panelOpened?" active":"")}>
+                            {/*<div id='search-sub-panel1' className={"ml-3 mr-3 mt-3".concat(this.state.panelOpened?" ":" display-none")}>*/}
+                            {/*    <EllipsisToolTip options={ellipsisToolTipOptions}>*/}
+                            {/*        {this.state.suggestions.length>0?*/}
+                            {/*        "users matching ".concat(this.state.searchInput)*/}
+                            {/*        :"can't find any user matching ".concat(this.state.searchInput)}*/}
+                            {/*    </EllipsisToolTip>*/}
+                            {/*</div>*/}
+                        <div id='search-sub-panel2' className={"search-result".concat(this.state.suggestions.length>0 && this.state.searchInput !== ""?" active":"")}>
+                            {this.state.panelOpened?
+                            this.state.suggestions.map(sug =>
+                                    <SearchUserResult email={sug.email} profile={sug.profile} isFriend={sug.isFriend}/>
+                                ):""}
+
+                            </div>
+                        </div>
                     </div>
-                    <button onClick={this.handelSearch}
+
+                    <button onClick={this.handleSearch}
                             className="shadow-none rounded-circle d-flex flex-row
                                     transparent-button btn-outline-primary">
                         {this.state.focused?
                         <svg xmlns="http://www.w3.org/2000/svg" width="27px" height="27px" fill="currentColor"
-                             className="bi bi-x" viewBox="0 0 16 16">
+                             className="bi bi-x align-self-center" viewBox="0 0 16 16">
                             <path
                                 d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
                         </svg>:
@@ -108,19 +133,7 @@ class SearchUser extends Component {
                             <path fillRule="evenodd" d="M6.5 12a5.5 5.5 0 1 0 0-11 5.5 5.5 0 0 0 0 11zM13 6.5a6.5 6.5 0 1 1-13 0 6.5 6.5 0 0 1 13 0z"/>
                         </svg>}
                     </button>
-                    <div id='panel' className={"mt-5 mr-2 ".concat(this.state.panelOpened?" active":"")}>
-                        <div id='search-sub-panel1' className={"m-3".concat(this.state.panelOpened?" ":" display-none")}>
-                            {this.state.searchInput.length<3?(this.state.minError?<p>Enter at least 3 letters!</p>:""):(this.state.suggestions.length>0?<p>Suggested Chatrooms :</p>:<p>can't suggest any Chatroom</p>)}
-                        </div>
-                        <div id='search-sub-panel2' className={"search-result".concat(this.state.suggestions.length>0 && this.state.searchInput !== ""?" active":"")}>
-                        {this.state.panelOpened?
-                        this.state.suggestions.map(sug =>
-                            <Link to={"/"}>
-                                <p className="mt-auto mb-auto">user</p>
-                            </Link>):""}
 
-                        </div>
-                    </div>
                 </div>
             </Fragment>
 
