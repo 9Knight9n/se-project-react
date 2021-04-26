25f3f5c20c49d1695bb9c3d143c3f2e6b59b93e3
Sajad <naghizadehmdsd@gmail.com>
2021-04-26 16:32:21 +0430

    fixed backgrounds

--- src/components/homepage/homepage.jsx
+++ src/components/homepage/homepage.jsx
@@ -3,6 +3,8 @@ import React, {Component, Fragment} from 'react';
 import './homepage.css'
 import {Route, Switch} from "react-router-dom";
 import search_bg from '../../assets/img/homepage-bg.jpg'
+import search_1_bg from '../../assets/img/homepage-bg-2.jpg'
+// import map_bg from '../../assets/img/homepage-bg.jpg'
 import { fromLonLat } from "ol/proj";
 import "ol/ol.css";
 
@@ -47,17 +49,27 @@ class Homepage extends Component {
         return (
             <div id='homepage' className="d-flex flex-column">
                 <div className={'w-100'} style={{height:this.state.numberOfPages+"00vh"}} >
-                    <div className={'homepage-div-bg'} style={{background:'url('+search_bg+')'}}>
+                    <div className={'homepage-div-bg'}
+                         style={{background:'url('+search_1_bg+')',borderRadius:'1.5rem'}}>
                         <div>
 
                         </div>
                     </div>
-                    <div className={'homepage-div-bg'} style={{backgroundColor:'white'}}>
-                        <RMap width={"100%"} height={"60vh"} initial={{ center: center, zoom: 11 }}>
-                            <ROSM />
-                        </RMap>
+                    <div className={'homepage-div-bg'} style={{backgroundColor:"white"}}>
+                        <div className={'row w-100 d-flex h-100'} >
+                            <div className={'col-md-8 col-lg-8 col-xl-8 col-sm-12 col-12 mt-auto mb-auto'}>
+                                <div className={'mr-5 ml-5'}>
+                                    <RMap  width={"100%"} height={"60vh"} initial={{ center: center, zoom: 11 }}>
+                                        <ROSM />
+                                    </RMap>
+                                </div>
+                            </div>
+                            <div className={'w-100 h-100 col-md-4 col-lg-4 col-xl-4 col-sm-12 col-12 mt-auto mb-auto'}>
+                                <h1 className={'mr-auto ml-auto'} style={{backgroundColor:'white'}}>Villa card here!</h1>
+                            </div>
+                        </div>
                     </div>
-                    <div className={'homepage-div-bg'} style={{backgroundColor:'black'}}>
+                    <div className={'homepage-div-bg'} style={{background:'url('+search_bg+')',borderRadius:'1.5rem'}}>
                         <div>
 
                         </div>
