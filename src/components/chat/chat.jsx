8f8a374e4fe94c12cfcdfc9238352def0d85da16
Sajad <naghizadehmdsd@gmail.com>
2021-06-03 02:30:40 +0430

    added upload file button and some other css

--- src/components/chat/chat.jsx
+++ src/components/chat/chat.jsx
@@ -51,7 +51,16 @@ class Chat extends Component {
                     </button>
                 </Affix>
                 <Drawer
-                    title="Chats"
+                    title={
+                        <h4 style={{fontFamily: 'cursive'}}>Chats:</h4>
+                    }
+                    closeIcon={
+                        <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#dc3545"
+                             className="bi bi-x-circle-fill " viewBox="0 0 16 16">
+                            <path
+                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+                        </svg>
+                    }
                     width={500}
                     closable={true}
                     onClose={this.onClose}
@@ -78,7 +87,14 @@ class Chat extends Component {
                     {/*    Two-level drawer*/}
                     {/*</button>*/}
                     <Drawer
-                        title="chatroom"
+                        // title="chatroom"
+                        closeIcon={
+                            <svg xmlns="http://www.w3.org/2000/svg" width="20" height="20" fill="#dc3545"
+                                            className="bi bi-x-circle-fill " viewBox="0 0 16 16">
+                                <path
+                                    d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM5.354 4.646a.5.5 0 1 0-.708.708L7.293 8l-2.647 2.646a.5.5 0 0 0 .708.708L8 8.707l2.646 2.647a.5.5 0 0 0 .708-.708L8.707 8l2.647-2.646a.5.5 0 0 0-.708-.708L8 7.293 5.354 4.646z"/>
+                            </svg>
+                        }
                         width={500}
                         closable={true}
                         onClose={this.onChildrenDrawerClose}
--- src/components/chat/chatroom/chatroom.jsx
+++ src/components/chat/chatroom/chatroom.jsx
@@ -277,7 +277,7 @@ class Chatroom extends Component {
     render() {
         return (
             <React.Fragment>
-                <div className="w-100 h-100 p-2">
+                <div className="w-100 h-100 p-2 mt-3">
                     <div id="question-page" className="d-flex flex-column h-100 w-100">
                         <div id="chatroom-info" className=" d-flex flex-row" style={{border:'solid 2px'}}>
                             <h1>
@@ -335,46 +335,70 @@ class Chatroom extends Component {
                                         <Input
                                             inputStyle={
                                                 {
+                                                    height:'95px',
                                                     width:'100%',
                                                     padding:'5px',
+                                                    paddingLeft:'10px',
+                                                    paddingRight:'10px',
                                                     resize:'none',
-                                                    overflowY:this.state.inputHeight<200?'hidden':'auto'
+                                                    overflowY:this.state.inputHeight<200?'hidden':'auto',
+                                                    border: 'none',
+                                                    backgroundColor: '#ebebeb',
+                                                    borderRadius:'0.5rem',
+                                                    boxShadow: 'none',
+                                                    outline:'none',
                                                 }
                                             }
+                                            className={'rounded'}
                                             ref={el => (this.state.inputRef = el)}
                                             // onChange={this.inputOnChange}
-                                            minHeight={57}
+                                            minHeight={95}
                                             placeholder="Type here..."
                                             multiline={true}
                                             autoHeight={true}
                                             autofocus={true}
                                             rightButtons={
-                                                <button
-                                                    className="btn btn-primary transparent-button mt-auto shadow-none ml-2 mb-2"
-                                                    // {this.state.inputValue.length===0?disabled:""}
-                                                    // onClick={this.sendMessage}
-                                                    id="generalChatroomSendButton"
-                                                    style={{color:'white',padding:'0'}}>
-                                                    <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"
-                                                         fill="#007bff" className="bi bi-telegram"
-                                                         viewBox="0 0 16 16">
-                                                        <path
-                                                            d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
-                                                    </svg>
-                                                </button>
+                                                <div className={'d-flex flex-column'}>
+                                                    <button
+                                                        className="btn btn-primary transparent-button mt-auto shadow-none ml-2 mb-2"
+                                                        // {this.state.inputValue.length===0?disabled:""}
+                                                        // onClick={this.sendMessage}
+                                                        // id="generalChatroomSendButton"
+                                                        style={{color:'white',padding:'0'}}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"
+                                                             fill="lightseagreen" className="bi bi-plus-circle-fill"
+                                                             viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.5 4.5a.5.5 0 0 0-1 0v3h-3a.5.5 0 0 0 0 1h3v3a.5.5 0 0 0 1 0v-3h3a.5.5 0 0 0 0-1h-3v-3z"/>
+                                                        </svg>
+                                                    </button>
+                                                    <button
+                                                        className="btn btn-primary transparent-button shadow-none ml-2 mb-2"
+                                                        // {this.state.inputValue.length===0?disabled:""}
+                                                        // onClick={this.sendMessage}
+                                                        id="generalChatroomSendButton"
+                                                        style={{color:'white',padding:'0'}}>
+                                                        <svg xmlns="http://www.w3.org/2000/svg" width="35" height="35"
+                                                             fill="#007bff" className="bi bi-telegram"
+                                                             viewBox="0 0 16 16">
+                                                            <path
+                                                                d="M16 8A8 8 0 1 1 0 8a8 8 0 0 1 16 0zM8.287 5.906c-.778.324-2.334.994-4.666 2.01-.378.15-.577.298-.595.442-.03.243.275.339.69.47l.175.055c.408.133.958.288 1.243.294.26.006.549-.1.868-.32 2.179-1.471 3.304-2.214 3.374-2.23.05-.012.12-.026.166.016.047.041.042.12.037.141-.03.129-1.227 1.241-1.846 1.817-.193.18-.33.307-.358.336a8.154 8.154 0 0 1-.188.186c-.38.366-.664.64.015 1.088.327.216.589.393.85.571.284.194.568.387.936.629.093.06.183.125.27.187.331.236.63.448.997.414.214-.02.435-.22.547-.82.265-1.417.786-4.486.906-5.751a1.426 1.426 0 0 0-.013-.315.337.337 0 0 0-.114-.217.526.526 0 0 0-.31-.093c-.3.005-.763.166-2.984 1.09z"/>
+                                                        </svg>
+                                                    </button>
+                                                </div>
                                             }
-                                            // leftButtons={this.state.replying?
-                                            //     <div className="black-text">
-                                            //         <button className="p-2 rounded replyToButton">
-                                            //             Replying to {this.state.replyingTo}
-                                            //         </button>
-                                            //         <button className="p-1" style={{backgroundColor:"transparent"}} onClick={()=>this.reply(null,null)}>
-                                            //             <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
-                                            //                 <path fillRule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
-                                            //             </svg>
-                                            //         </button>
-                                            //     </div>:""
-                                            // }
+                                            leftButtons={this.state.replying?
+                                                <div className="black-text">
+                                                    <button className="p-2 rounded replyToButton">
+                                                        Replying to {this.state.replyingTo}
+                                                    </button>
+                                                    <button className="p-1" style={{backgroundColor:"transparent"}} onClick={()=>this.reply(null,null)}>
+                                                        <svg width="1em" height="1em" viewBox="0 0 16 16" className="bi bi-x" fill="currentColor" xmlns="http://www.w3.org/2000/svg">
+                                                            <path fillRule="evenodd" d="M4.646 4.646a.5.5 0 0 1 .708 0L8 7.293l2.646-2.647a.5.5 0 0 1 .708.708L8.707 8l2.647 2.646a.5.5 0 0 1-.708.708L8 8.707l-2.646 2.647a.5.5 0 0 1-.708-.708L7.293 8 4.646 5.354a.5.5 0 0 1 0-.708z"/>
+                                                        </svg>
+                                                    </button>
+                                                </div>:""
+                                            }
                                         />
                                     </div>
                                 </ResizeObserver>
