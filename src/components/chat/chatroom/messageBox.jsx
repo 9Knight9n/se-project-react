87287e9ddced2e382585fb9ff02980b04c011033
Sajad <naghizadehmdsd@gmail.com>
2021-06-10 19:55:23 +0430

    fixed some css

--- src/components/chat/chatroom/messageBox.jsx
+++ src/components/chat/chatroom/messageBox.jsx
@@ -1,33 +1,69 @@
-import React, { Component } from 'react';
+import React from 'react'
+import { MessageBox as MB } from 'react-chat-elements'
+// import { Avatar } from 'react-chat-elements'
+// import { getUserAvatar } from './util';
+import { Dropdown } from 'react-bootstrap';
+// CE CSS
+import 'react-chat-elements/dist/main.css';
+// MessageBox component
 import './messageBox.css';
-class MessageBox extends Component {
-    state = {
+// import ProfilePreview from './ProfilePreview';
+
+
+class MessageBox extends React.Component {
+    state={
+        showProfilePreview:false,
+        isOwner:this.props.userid===parseInt(sessionStorage.getItem("id")),
+        message_id:this.props.message_id,
+        userid:this.props.userid,
+        title:this.props.title,
+        text:this.props.text,
+        dateString:this.props.dateString,
+        isReply:this.props.isReply,
+        titleRep:this.props.titleRep,
+        messageRep:this.props.messageRep
     }
+
+    async componentDidMount(){
+    }
+
+
+
+
     render() {
         return (
-            <div className="comment d-flex flex-column">
-                <div className="d-flex flex-row">
-                    <div className="d-flex align-items-center mr-2"><img  id="profile-img-in-chat"
-                                                                          src="https://moonvillageassociation.org/wp-content/uploads/2018/06/default-profile-picture1.jpg"/></div>
-                    <h1 className="d-flex align-items-center"><p><a href="">{this.props.chat.by}</a></p></h1>
-                </div>
-                <div className="m-3 handle-enter"><p>{this.props.chat.content}</p></div>
-                <div className="row">
-                    <div className="col-lg-6 d-flex justify-content-start">
-                        <em>{this.props.chat.time}</em>
-                    </div>
-                    <div className="col-lg-6 d-flex justify-content-end">
-                        <p>
-                            <a onClick={() => this.props.showModal(this.props.chat.id)} href="#">
-                                reply
-                            </a>
-                        </p>
-                    </div>
-                </div>
-            </div>
 
+            <React.Fragment>
+                <div className={'w-fit-content d-flex w-100 flex-row'.concat(this.state.isOwner?"-reverse":"")}>
+
+                    <div className='w-100'>
+                        <MB
+                            id={this.state.id}
+                            position={this.state.isOwner?"right":'left'}
+                            type={'text'}
+                            // title={!this.state.isOwner?this.state.title:""}
+                            titleColor='#8717ae'
+                            text={this.state.text}
+                            dateString={this.state.dateString}
+                            // onReplyClick={()=>this.props.reply(this.state.message_id,this.state.title)}
+                            // onClick
+                            onReplyMessageClick={() => this.props.onReplyMessageClick(this.state.message_id)}
+                            // replyButton={true}
+                            reply={this.state.isReply?{
+                                title:this.state.titleRep,
+                                titleColor: '#8717ae',
+                                message:this.state.messageRep,
+                            }:false}
+
+                            // reply
+                        />
+                    </div>
+
+                </div>
+
+            </React.Fragment>
         );
     }
 }
-
 export default MessageBox;
+
