f16441e0cfd894f96d6841728cb6cd1f4894a7de
Sajad <naghizadehmdsd@gmail.com>
2021-07-10 03:32:33 +0430

    added firebase

--- src/App.js
+++ src/App.js
@@ -6,11 +6,20 @@ import PageRouter from "./components/pageRouter";
 import {ToastContainer} from "react-toastify";
 import {eventViewport} from "./components/util";
 import Chat from "./components/chat/chat";
+import {sendToken, getMessaging} from './components/firebase'
+import {STORAGE_KEY, WEB_PUSH_CERTIFICATE} from "./components/constants";
 
 class App extends Component {
     componentDidMount() {
         window.onresize = () => eventViewport();
         window.onload = () => eventViewport();
+
+
+
+        let msg = getMessaging();
+        msg.onMessage((payload)=>{
+            console.log('message:',payload)
+        });
     }
 
     render() {
--- src/components/firebase.jsx
+++ src/components/firebase.jsx
@@ -1,7 +1,5 @@
-import firebase from "firebase/app";
-import 'firebase/messaging';
-import "firebase/auth";
-import {WEB_PUSH_CERTIFICATE} from "./constants";
+import firebase from "firebase";
+import {STORAGE_KEY, WEB_PUSH_CERTIFICATE} from "./constants";
 
 const firebaseConfig = {
     apiKey: "AIzaSyBPkNtNdBHnjC-QJ9dbrnFQO3rcf1rwYRk",
@@ -9,40 +7,37 @@ const firebaseConfig = {
     projectId: "sweet-home-773bb",
     storageBucket: "sweet-home-773bb.appspot.com",
     messagingSenderId: "872929672242",
-    appId: "1:872929672242:web:2b04bc0040f91495c6d02b",
-    measurementId: "G-5RQ6W8YPE5",
+    appId: "1:872929672242:web:4ca7dc26e3bc6490c6d02b",
+    measurementId: "G-Z71M4YXBX4"
 };
 
-// if (!firebase)
-    firebase.initializeApp(firebaseConfig);
+firebase.initializeApp(firebaseConfig);
 
 const messaging = firebase.messaging();
 
+messaging.requestPermission()
+    .then(()=>{
+        console.log("push notification permission accepted.")
+        return messaging.getToken({vapidKey:WEB_PUSH_CERTIFICATE})
+    }).then((token)=>{
+    // write code to send to server
+    sessionStorage.setItem(STORAGE_KEY+'firebase-token',token)
+    console.log(token)
+}).catch(()=> {
+    console.log("push notification permission denied.")
+})
 
-export const onMessageListener = () =>
-    new Promise((resolve) => {
-        messaging.onMessage((payload) => {
-            console.log(payload)
-            resolve(payload);
-        });
-    });
 
-
-export const getToken = (setTokenFound) => {
-    return messaging.getToken({vapidKey: WEB_PUSH_CERTIFICATE}).then((currentToken) => {
-        if (currentToken) {
-            console.log('current token for client: ', currentToken);
-            setTokenFound(true);
-            // Track the token -> client mapping, by sending to backend server
-            // show on the UI that permission is secured
-        } else {
-            console.log('No registration token available. Request permission to generate one.');
-            setTokenFound(false);
-            // shows on the UI that permission is required
-        }
-    }).catch((err) => {
-        console.log('An error occurred while retrieving token. ', err);
-        // catch error while creating client token
-    });
+export const getMessaging = () => {
+    return messaging
 }
+
+// export const onMessageListener = () =>
+//     new Promise((resolve) => {
+//         messaging.onMessage((payload) => {
+//             console.log(payload)
+//             resolve(payload);
+//         });
+//     });
+
 
